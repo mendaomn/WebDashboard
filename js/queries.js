50c1667e336bf5f08a5508d00a1c746183242392
Alessandro MEnduni <menduni@eurecom.fr>
2015-04-17 12:09:22 +0200

    Sources logos added
    Need of adapting query result to source(s) selected

--- js/queries.js
+++ js/queries.js
@@ -1,12 +1,11 @@
 function countGivenSource() {
     var URL = "http://3cixty.eurecom.fr/sparql?default-graph-uri=&";
-    var source = "http://www.facebook.com";
+    var source = "http://www.yelp.com";
     var obj = {
         query : "select count (?x) where { ?x a dul:Place . ?x dc:publisher ?pub. FILTER regex(?pub, \""+source+"\")}" 
     };
     //This query counts places coming from "source"
-   //$.getJSON(URL, obj, function( data ){
-        //$('#count_result').html(data["results"]["bindings"]["0"]["callret-0"]["value"]);
-   //});   
-    $('#count_result').html(12);
+   $.getJSON(URL, obj, function( data ){
+        $('#count_result').html(data["results"]["bindings"]["0"]["callret-0"]["value"]);
+   });   
 }
