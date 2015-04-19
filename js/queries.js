2248fc12da62dfe1fff61a4619383ff10db51309
Alessandro Menduni <menduni@eurecom.fr>
2015-04-19 10:55:04 +0200

    Aside styling and behaviour
    Queries for selected sources

--- js/queries.js
+++ js/queries.js
@@ -1,10 +1,18 @@
 function countGivenSource() {
+    // Extracts selected values from select element
+    var selectedSources = $('select').find(":selected").map(function(i, e){
+        return $(this).val();
+    });
     var URL = "http://3cixty.eurecom.fr/sparql?default-graph-uri=&";
-    var source = "http://www.yelp.com";
     var obj = {
-        query : "select count (?x) where { ?x a dul:Place . ?x dc:publisher ?pub. FILTER regex(?pub, \""+source+"\")}" 
-    };
-    //This query counts places coming from "source"
+        query : "select count (?x) where { ?x a dul:Place . "
+    };         
+    $.each(selectedSources, function(i, e){
+        newpub = "?x dc:publisher ?pub"+i+". FILTER regex(?pub"+i+", \"http://www."+e+".com\")";
+        obj.query += newpub;
+    });
+    obj.query += "}";
+    //This query counts places coming from a "source"
    $.getJSON(URL, obj, function( data ){
         $('#count_result').html(data["results"]["bindings"]["0"]["callret-0"]["value"]);
    });   
